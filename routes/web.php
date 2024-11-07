4739a0fcc16f6acca56424fb8f761939a9f77bee
vladlen-beilik <vladlen.beilik@gmail.com>
2024-11-07 07:59:10 +0100

    ref

--- routes/web.php
+++ routes/web.php
@@ -5,36 +5,3 @@ use Illuminate\Support\Facades\Route;
 //Route::get('', [\Sambu\Ecommerce\Http\Controllers\HomeController::class, 'index'])
 //    ->name('home.index');
 //
-//Route::controller(\Sambu\Ecommerce\Http\Controllers\ProductCategoryController::class)
-//    ->prefix('product-categories')
-//    ->name('product-categories.')
-//    ->group(function () {
-//        Route::get('', 'index')->name('index');
-//        Route::get('{slug}', 'show')->name('show');
-//    });
-//
-//Route::controller(\Sambu\Ecommerce\Http\Controllers\ProductBrandController::class)
-//    ->prefix('product-brands')
-//    ->name('product-brands.')
-//    ->group(function () {
-//        Route::get('', 'index')->name('index');
-//        Route::get('{slug}', 'show')->name('show');
-//    });
-//
-//Route::controller(\Sambu\Ecommerce\Http\Controllers\ProductController::class)
-//    ->prefix('products')
-//    ->name('products.')
-//    ->group(function () {
-//        Route::get('{slug}', 'show')->name('show');
-//        Route::get('{slug}/attributes', 'showAttribute')->name('showAttribute');
-//        Route::get('{slug}/comments', 'showComments')->name('showComments');
-//    });
-//
-//Route::controller(\Sambu\Ecommerce\Http\Controllers\CheckoutController::class)
-//    ->prefix('checkout')
-//    ->name('checkout.')
-//    ->group(function () {
-//        Route::get('', 'index')->name('index');
-//    });
-
-
